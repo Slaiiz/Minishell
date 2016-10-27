0f3250fca83173f9ed282c6abb91190aac875056
Slaiiz <crocoshack@gmail.com>
2016-10-27 19:12:39 +0200

    Errors handled better.

--- src/builtins/unsetenv.c
+++ src/builtins/unsetenv.c
@@ -6,7 +6,7 @@
 /*   By: vchesnea <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/09 17:11:48 by vchesnea          #+#    #+#             */
-/*   Updated: 2016/10/09 17:11:48 by vchesnea         ###   ########.fr       */
+/*   Updated: 2016/10/27 17:26:06 by vchesnea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,5 +25,5 @@ void	builtin_unsetenv(int argc, char **argv)
 		return ;
 	}
 	if (delete_var(argv[1]))
-		ft_printf("%s\n", get_error());
+		ft_printf("%s: %s\n", argv[0], get_error());
 }
