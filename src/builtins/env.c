2cef9354968dcd6ff5e41ed58e9b32df97f7fa1b
Slaiiz <crocoshack@gmail.com>
2016-10-27 19:10:51 +0200

    Envs are now listed in the right order.

--- src/builtins/env.c
+++ src/builtins/env.c
@@ -6,27 +6,30 @@
 /*   By: vchesnea <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/09 17:11:34 by vchesnea          #+#    #+#             */
-/*   Updated: 2016/10/09 17:11:35 by vchesnea         ###   ########.fr       */
+/*   Updated: 2016/10/27 15:01:12 by vchesnea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "private/execute.h"
+
+static void	recurse(const t_entry *env)
+{
+	if (env->next)
+		recurse(env->next);
+	ft_printf("%s=%s\n", env->key, env->value);
+}
 
 /*
 ** Builtin command : env
 ** Prints the list of environment variables on the standard output.
 */
 
-void	builtin_env(int argc, char **argv)
+void		builtin_env(int argc, char **argv)
 {
 	const t_entry	*env;
 
 	env = get_vars();
-	while (env != NULL)
-	{
-		ft_printf("%s=%s\n", env->key, env->value);
-		env = env->next;
-	}
+	recurse(env);
 	if (argc)
 		++argv;
 }
--- src/builtins/setenv.c
+++ src/builtins/setenv.c
@@ -6,11 +6,18 @@
 /*   By: vchesnea <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/09 17:11:44 by vchesnea          #+#    #+#             */
-/*   Updated: 2016/10/09 17:11:44 by vchesnea         ###   ########.fr       */
+/*   Updated: 2016/10/27 17:06:43 by vchesnea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "private/execute.h"
+
+static void	recurse(const t_entry *env)
+{
+	if (env->next)
+		recurse(env->next);
+	ft_printf("%s=%s\n", env->key, env->value);
+}
 
 /*
 ** Builtin command : setenv
@@ -19,20 +26,15 @@
 ** Prints the list of environment variables on the standard output.
 */
 
-void	builtin_setenv(int argc, char **argv)
+void		builtin_setenv(int argc, char **argv)
 {
 	const t_entry	*env;
 
 	if (argc < 2)
 	{
 		env = get_vars();
-		while (env != NULL)
-		{
-			ft_printf("%s=%s\n", env->key, env->value);
-			env = env->next;
-		}
+		recurse(env);
 		return ;
 	}
-	ft_printf("%s\n%s\n", argv[1], argv[2]);
 	set_var(argv[1], argc < 3 ? "" : argv[2]);
 }
