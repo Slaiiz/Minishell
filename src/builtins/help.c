605dfa562dfe0f2f2ad658bc3a912b9ba3cd84c1
Slaiiz <crocoshack@gmail.com>
2016-10-29 19:13:00 +0200

    Improved help command.

--- src/builtins/help.c
+++ src/builtins/help.c
@@ -6,7 +6,7 @@
 /*   By: vchesnea <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/09 17:09:56 by vchesnea          #+#    #+#             */
-/*   Updated: 2016/10/09 17:10:11 by vchesnea         ###   ########.fr       */
+/*   Updated: 2016/10/29 18:59:04 by vchesnea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,5 @@ void	builtin_help(int argc, char **argv)
 {
 	if (argc)
 		++argv;
-	ft_printf("Usage: ./minishell [-h] [--help]\n");
+	ft_printf("cd, env, exit, help, pwd, setenv, unsetenv\n");
 }
