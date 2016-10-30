f8a8bc3d60f68400bcdf3629340195432b10af26
Slaiiz <crocoshack@gmail.com>
2016-10-30 18:19:33 +0100

    Added function substitute_vars.

--- src/helpers.c
+++ src/helpers.c
@@ -6,11 +6,50 @@
 /*   By: vchesnea <vchesnea@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/30 10:03:55 by vchesnea          #+#    #+#             */
-/*   Updated: 2016/10/30 10:14:44 by vchesnea         ###   ########.fr       */
+/*   Updated: 2016/10/30 18:12:10 by vchesnea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "private/helpers.h"
+
+#include "error.h"
+#include "vars.h"
+
+/*
+** Replaces every occurence of $[a-zA-Z]+ with the value of the environment
+** variable refered by it.
+*/
+
+char		*substitute_vars(const char *string)
+{
+	t_buff		*buf;
+	const char	*ptr;
+	char		*tmp;
+
+	buf = ft_bufnew(32);
+	if (buf == NULL)
+		return (NULL);
+	while ((ptr = ft_strchr(string, '$')) && ++ptr)
+	{
+		if (ft_bufadd(buf, string, ptr - string))
+			return (NULL);
+		string = ptr;
+		while (ft_isalnum(*ptr) || *ptr == '_')
+			++ptr;
+		tmp = ft_strsub(string, 0, ptr - string);
+		if (tmp == NULL)
+			return (NULL);
+		ft_bufadd(buf, get_var(tmp), ft_strlen(get_var(tmp)));
+		string = ptr;
+		free(tmp);
+	}
+	if (ft_bufadd(buf, string, ft_strchr(string, '\0') - string + 1))
+		return (NULL);
+	if ((tmp = ft_strdup(buf->data)) == NULL)
+		return (NULL);
+	ft_bufdel(&buf);
+	return (tmp);
+}
 
 /*
 ** Joins s1 and s2 together to form a path string.
@@ -18,7 +57,7 @@
 **  Returns a pointer to the new string on success, or NULL on failure.
 */
 
-char	*join_path(const char *s1, const char *s2)
+char		*join_path(const char *s1, const char *s2)
 {
 	size_t	len;
 	char	*new;
@@ -40,7 +79,7 @@ char	*join_path(const char *s1, const char *s2)
 **  Returns 1 when condition is met, or 0 in all other cases.
 */
 
-int		file_exists(const char *path)
+int			file_exists(const char *path)
 {
 	struct stat stats;
 
